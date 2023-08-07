95e62eb98d1bd52a46ef1fb97656eb30cf91f50a
Shingetsu592 <139620730+Shingetsu592@users.noreply.github.com>
2023-08-07 10:52:43 +0700

    Update and rename Installation.sh to Installation_bash.sh
    ROS, ARDUPILOT, MAVROS, and MAVLINK cheat installation

--- Installation_bash.sh
+++ Installation_bash.sh
@@ -2,7 +2,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo apt install ros-noetic-desktop
+sudo apt install ros-noetic-desktop-full
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential

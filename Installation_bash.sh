ffd3b7a37519621774c81ed7cc2bb429f0ce6b07
Shingetsu592 <139620730+Shingetsu592@users.noreply.github.com>
2023-08-07 11:54:56 +0700

    Update Installation_bash.sh

--- Installation_bash.sh
+++ Installation_bash.sh
@@ -1,3 +1,7 @@
+# ROS Full Deskop Installation
+# Copter-4.3.7
+# using ~/.bashrc
+
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
@@ -23,3 +27,5 @@ sudo apt-get install ros-noetic-mavros ros-noetic-mavros-extras
 wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
 chmod a+x install_geographiclib_datasets.sh
 ./install_geographiclib_datasets.sh
+sudo apt update
+sudo apt install python3-opencv

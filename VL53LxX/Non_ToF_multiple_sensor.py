35016239ad58ffae063834819ff009088f3414f8
Shingetsu592 <139620730+Shingetsu592@users.noreply.github.com>
2023-09-05 14:44:08 +0700

    Update Non_ToF_multiple_sensor.py

--- VL53LxX/Non_ToF_multiple_sensor.py
+++ VL53LxX/Non_ToF_multiple_sensor.py
@@ -1,32 +1,32 @@
 import time
 import board
-from digitalio import DigitalInOut
-from adafruit_vl53l0x import VL53L0X
+from digitalio import DigitalInOut as DigIO
+from adafruit_vl53l0x import untof
 
 i2c = board.I2C()
 
 xshut = [
-    DigitalInOut(board.D17),
-    DigitalInOut(board.D27),
-    DigitalInOut(board.D22)
+    DigIO(board.D17),
+    DigIO(board.D27),
+    DigIO(board.D22)
 ]
 
 for power_pin in xshut:
     power_pin.switch_to_output(value=False)
 
-vl53 = []
+vl53l0x = []
 
 for i, power_pin in enumerate(xshut):
     power_pin.value = True
-    vl53.insert(i, VL53L0X(i2c))
-    vl53[i].start_continous()
+    vl53l0x.insert(i, untof(i2c))
+    vl53l0x[i].start_continous()
     if i < len(xshut) - 1:
-        vl53[i].set_address(i + 0x30)
+        vl53l0x[i].set_address(i + 0x30)
 
 def detect_range(count=5):
     """take count=5 samples"""
     while count:
-        for index, sensor in enumerate(vl53):
+        for index, sensor in enumerate(vl53l0x):
             print("Sensor {} Range: {}mm".format(index + 1, sensor.range))
         time.sleep(1.0)
         count -= 1
